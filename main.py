42812b5e732dc1bcfd3fe9d701a006fa5ebdd31d
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-03-16 01:38:44 -0400

    main in main

--- main.py
+++ main.py
@@ -1,57 +1,65 @@
 from copy import deepcopy
+from PIL import Image
 import time
 import random; random.seed(1)
 import numpy as np
-from PIL import Image
 import NeuralNetwork as nn
 import Debounce as D
-print('Imports Sucessfull')
 
-def grab(uuid, path):
-	f = Image.open(f'{path}{uuid}.tif')
-	a = np.array(f)
-	return [1-row/255 for row in np.reshape(a, (27648))]
+if __name__ == '__main__':
+	def grabRGB(uuid, path, size):
+		f = Image.open(f'{path}{uuid}.tif').resize(size)
+		a = np.array(f)
+		return [1-row/255 for row in np.reshape(a, size[0]*size[1])]
 
-path = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\YRSTF2021\\Data\\'
-labelsFile = open(f'{path}train_labels.csv')
-labels = [row.split(',') for row in labelsFile.read().split('\n')][1:]
-labelsFile.close()
+	def grabG(uuid, path, size):
+		f = Image.open(f'{path}{uuid}.tif').resize(size)
+		a = np.array([[1-sum(col)/765 for col in row] for row in np.array(f)])
+		return np.reshape(a, size[0]*size[1])
 
-print('Data Read')
+	print('Imports Sucessfull')
 
-dx = 0.001
-rate = 0.1
-beta = 0.9
-scale = 0.1
-layerData = [32, 32, 32, len(labels[0][1])]
-weights = nn.generateWeights(layerData, 27648)
+	path = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\YRSTF2021\\Data\\'
+	labelsFile = open(f'{path}train_labels.csv')
+	labels = [row.split(',') for row in labelsFile.read().split('\n')][1:]
+	labelsFile.close()
 
-print('Variables Initialized')
-open(f'{path}scores.csv', 'w+').truncate(0)
+	print('Data Read')
 
-print('Training...')
-start = time.time()
+	dx = 1.001
+	rate = 0.1
+	beta = 0.9
+	scale = 0.1
+	size = (6, 6)
+	layerData = [32, 32, 32, len(labels[0][1])]
+	weights = nn.generateWeights(layerData, size[0]*size[1])
 
-num, cost = 0, 0
-for row in labels:
-	inputs = grab(row[0], f'{path}train\\')
-	outputs = int(row[1])
+	print('Variables Initialized')
+	open(f'{path}scores.csv', 'w+').truncate(0)
 
-	weights, newOutputs = D.backPropagation(inputs, weights, outputs, dx, rate, beta, scale)
+	print('Training...')
 
-	cost += nn.neuralNetworkCost(inputs, weights, outputs)
-	t = int(time.time() - start)
+	start = time.time()
 
-	if not (inp+1)%10:
-		print('\n\nNetwork:', num+1)
-		print(f'Time: {t}s')
-		print('Cost:', cost/10)
-		print('\nPred:', newOutputs)
-		print('Real:', outputs)
+	num, cost = 0, 0
+	for row in labels:
+		inputs = grabG(row[0], f'{path}train\\', size)
+		outputs = int(row[1])
 
-		f = open(f'{path}scores.csv', 'a')
-		f.write(f'\n{cost/10}'); f.close()
-		cost = 0
+		weights, newOutputs = D.backPropagation(inputs, weights, outputs, dx, rate, beta, scale)
+		np.save(f'{path}weights.npy', np.array(weights, dtype=object))
 
-	np.save(f'{path}weights.npy', np.array(weights, dtype=object))
-	num += 1
+		cost += nn.neuralNetworkCost(inputs, weights, outputs)
+
+		if not (num+1)%1:
+			print('\n\nNetwork:', num+1)
+			print(f'Time: {time.time() - start}s')
+			print('Cost:', cost/1)
+			print('\nPred:', newOutputs)
+			print('Real:', outputs)
+
+			f = open(f'{path}scores.csv', 'a')
+			f.write(f'\n{cost/1}'); f.close()
+			cost = 0
+
+		num += 1
