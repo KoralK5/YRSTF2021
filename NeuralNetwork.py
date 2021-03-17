e5191eb08d776258fe7b78661222ae97cc80b56e
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-03-16 20:40:29 -0400

    nopython=True

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,27 +1,30 @@
 import numpy as np
 from copy import deepcopy
+from numba import njit
 
 def generateWeights(layerData, inputQuantity):
 	layerDepthLimit = len(layerData)
 	augmentedLayerData = [inputQuantity] + deepcopy(layerData)
 	return [np.random.rand(layerData[layerDepth], augmentedLayerData[layerDepth] + 1) - 0.5 for layerDepth in range(layerDepthLimit)]
 
+@njit(nopython=True)
 def softmax(x):
 	return np.exp(x - np.max(x)) / np.exp(x - np.max(x)).sum()
 
+@njit(nopython=True)
 def ReLU(x):
 	return max(x, 0)
 
+@njit(nopython=True)
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
 def layer(inputs, weights):
 	biasedInputs = np.append(np.array(inputs), 1)
 	neuronInputs = np.repeat(np.array([biasedInputs]), len(weights), axis = 0)
-	weightedInputs = np.sum(neuronInputs * np.array(weights), 1)
-
+	
 	actFunc = np.vectorize(sigmoid)
-	weightedInputs = actFunc(weightedInputs)
+	weightedInputs = actFunc(np.sum(neuronInputs * np.array(weights), 1))
 
 	return weightedInputs
 
@@ -35,6 +38,6 @@ def neuralNetwork(inputs, weights):
 
 def layerCost(inputs, weights, outputs):
 	return np.sum((outputs - layer(inputs, weights)) ** 2)
-		
+
 def neuralNetworkCost(inputs, weights, outputs):
 	return np.sum((outputs - neuralNetwork(inputs, weights)[-1]) ** 2)
