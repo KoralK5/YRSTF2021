b46820751338debf2e086b94e7f3aeaac1ada894
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-03-20 21:09:09 -0400

    Update tensorNN.py

--- tensorNN.py
+++ tensorNN.py
@@ -1,57 +1,81 @@
-from copy import deepcopy
-from PIL import Image
-import numpy as np
-from os import system
-import random; random.seed(1)
-import tensorflow as tf
-print('Imports Sucessfull')
+import NeuralNetwork as nn
+from numpy import tanh
+import _pickle as cPickle
 
-def grab(uuid, path):
-	f = Image.open(f'{path}{uuid}.tif')
-	a = np.array([[1-sum(col)/765 for col in row] for row in np.array(f)])
-	return a
+class Layer:
+	def __init__(self, inputs, weights, outputs, dx):
+		self.inputs = inputs
+		self.weights = weights
+		self.outputs = outputs
+		self.dx = dx
 
-path = 'D:\\Users\\Koral Kulacoglu\\Coding\\python\\AI\\YRSTF2021\\Data\\'
-labelsFile = open(f'{path}train_labels.csv')
-labels = [row.split(',') for row in labelsFile.read().split('\n')][1:]
-labelsFile.close()
+	def adjustWeight(self, neuron, weight):
+		newWeights = cPickle.loads(cPickle.dumps(self.weights, -1))
+		newWeights[neuron][weight] += self.dx
+		return (nn.layerCost(self.inputs, newWeights, self.outputs) - nn.layerCost(self.inputs, self.weights, self.outputs)) / self.dx
 
-inputs, outputs = [], []
-for row in range(len(labels)):
-	inputs.append(grab(labels[row][0], f'{path}train\\'))
-	outputs.append([int(labels[row][1]), abs(1-int(labels[row][1]))])
-	row += 1
+	def adjustNeuron(self, neuron):
+		newInputs = cPickle.loads(cPickle.dumps(self.inputs, -1))
+		newInputs[neuron] += self.dx
+		return (nn.layerCost(newInputs, self.weights, self.outputs) - nn.layerCost(self.inputs, self.weights, self.outputs)) / self.dx
+		
+	def adjustLayer(self, rate, beta, scale, velW, velI, preVelW, preVelI):
+		newWeights = cPickle.loads(cPickle.dumps(self.weights, -1))
+		newInputs = cPickle.loads(cPickle.dumps(self.inputs, -1))
+		for neuron in range(len(self.weights)):
+			for weight in range(len(self.weights[neuron])):
+				gradient = self.adjustWeight(neuron, weight)
+				velW = (beta - scale * tanh(velW - preVelW)) * velW + gradient * rate
 
-	system('cls')
-	print('Reading Data')
-	print(f'{int(row/len(labels)*100)}% - {row}/{len(labels)}')
-	print('⬜'*int(row/len(labels)*10) +'⬛'*(10-int(row/len(labels)*10)))
+				newWeights[neuron][weight] -= (beta - scale * tanh(velW - preVelW)) * velW + rate * gradient
 
-system('cls')
-print('Data Read')
+				preVelW = velW
 
-dx = 1.001
-rate = 0.1
-beta = 0.9
-scale = 0.1
-size = (96, 96)
-layerData = [24, 13, 8, 2]
+		for neuron in range(len(self.inputs)):
+			gradient = self.adjustNeuron(neuron)
+			velI = (beta - scale * tanh(velI - preVelI)) * velI + gradient * rate
 
-model = tf.keras.models.Sequential([
-	tf.keras.layers.Flatten(input_shape=(size)),
-	tf.keras.layers.Dense(128, activation='relu'),
-	tf.keras.layers.Dropout(0.2),
-	tf.keras.layers.Dense(10)
-])
-print('Parameters Set')
+			newInputs[neuron] -= (beta - scale * tanh(velI - preVelI)) * velI + rate * gradient
 
-predictions = model(inputs[:1]).numpy()
-tf.nn.softmax(predictions).numpy()
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+			preVelI = velI
+		return newWeights, newInputs
 
-model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
+class Network:
+	def __init__(self, inputs, weights, outputs, dx):
+		self.inputs = inputs
+		self.weights = weights
+		self.outputs = outputs
+		self.dx = dx
 
-model.fit(inputs, outputs, epochs=5)
-model.evaluate(inputs,  outputs, verbose=2)
+	def adjustWeight(self, layer, neuron, weight):
+		newWeights = cPickle.loads(cPickle.dumps(self.weights, -1))
+		newWeights[layer][neuron][weight] += self.dx
+		return (nn.neuralNetworkCost(self.inputs, newWeights, self.outputs) - nn.neuralNetworkCost(self.inputs, self.weights, self.outputs)) / self.dx
+
+	def adjustNeuron(self, layer, neuron):
+		newInputs = cPickle.loads(cPickle.dumps(self.inputs, -1))
+		newInputs[layer][neuron] += self.dx
+		return (nn.neuralNetworkCost(newInputs, self.weights, self.outputs) - nn.neuralNetworkCost(self.inputs, self.weights, self.outputs)) / self.dx
+		
+	def adjustNetwork(self, rate):
+		newWeights = cPickle.loads(cPickle.dumps(self.weights, -1))
+		for layer in range(len(self.weights)):
+			for neuron in range(len(self.weights[layer])):
+				for weight in range(len(self.weights[layer][neuron])):
+					newWeights[layer][neuron][weight] -= self.adjustWeight(layer, neuron, weight) * rate
+		return newWeights
+
+def backPropagation(inputs, weights, outputs, dx, rate, beta, scale):
+	newWeights = cPickle.loads(cPickle.dumps(weights[::-1], -1))
+	newOutputs = cPickle.loads(cPickle.dumps(outputs, -1))
+	
+	inps = nn.neuralNetwork(inputs, weights)
+
+	networkInputs = inps[-2::-1] + [cPickle.loads(cPickle.dumps(inputs, -1))]
+
+	velW, velI = 0, 0
+	preVelW, preVelI = 0, 0
+	for currentLayer in range(len(networkInputs)):
+		layer = Layer(networkInputs[currentLayer], newWeights[currentLayer], newOutputs, dx)
+		newWeights[currentLayer], newOutputs = layer.adjustLayer(rate, beta, scale, velW, velI, preVelW, preVelI)
+	return newWeights[::-1], inps[-1]
