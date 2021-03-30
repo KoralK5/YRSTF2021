71dbae36f5476c79d6736daef9a6bb1f8ff232c4
KoralK5 <62809012+KoralK5@users.noreply.github.com>
2021-03-29 23:23:51 -0400

    it worky?

--- NNrewrite.py
+++ NNrewrite.py
@@ -1,34 +1,37 @@
 import numpy as np
-import _pickle
+import _pickle as pkl
 from numba import jit
 
-def deepcopy(x):
-	return _pickle.loads(_pickle.dumps(x))
 
 @jit(nopython=True)
 def softmax(x):
 	return np.exp(x - np.max(x)) / np.exp(x - np.max(x)).sum()
 
+
 @jit(nopython=True)
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
-def generateWeights(layerData, inputQuantity):
+def deepcopy(x):
+	return pkl.loads(pkl.dumps(x))
+
+
+def generateWeights(layerData, inputQuantity, minimum = -1, maximum = 1):
 	layerDepthLimit = len(layerData)
 	augmentedLayerData = [inputQuantity] + deepcopy(layerData)
-	return [np.random.rand(layerData[layerDepth], augmentedLayerData[layerDepth] + 1) - 0.5 for layerDepth in range(layerDepthLimit)]
+	return [np.random.uniform(minimum, maximum, [layerData[layerDepth], augmentedLayerData[layerDepth] + 1]) - 0.5 for layerDepth in range(layerDepthLimit)]
+
 
 def layer(inputs, weights, **kwargs):
 	if 'actFunc' in kwargs:
 		actFunc = kwargs['actFunc']
 	else:
 		actFunc = sigmoid
-	
-	shape = list(inputs.shape)
-	shape[-1] = 1
-	bias = np.full(shape, 1)
+	shape = list(inputs.shape)[:-1] + [1]
+	biases = np.full(shape, 1)
+	outputs = actFunc(np.append(inputs, biases, axis = len(shape) - 1) @ weights.transpose(np.roll(np.arange(len(weights.shape)), -1)))
+	return outputs.transpose(np.roll(np.arange(len(outputs.shape)), 1))
 
-	return actFunc(np.append(inputs, bias, axis = len(shape) - 1) @ weights.transpose())
 
 def neuralNetwork(inputs, weights, **kwargs):
 	if 'actFunc' in kwargs:
@@ -47,6 +50,13 @@ def neuralNetwork(inputs, weights, **kwargs):
 	neuronOutputs.append(weights[-1])
 	return neuronOutputs
 
+
+def dxMaker(weights, dx):
+	neuronAmount, weightAmount = len(weights), len(weights[0])
+	totalWeightAmount = neuronAmount * weightAmount
+	return np.reshape(np.tile(np.append(dx, np.zeros((totalWeightAmount))), totalWeightAmount)[:totalWeightAmount ** 2], (neuronAmount, weightAmount, neuronAmount, weightAmount)) + weights
+
+
 def optimizer(inputs, weights, outputs, dx, **kwargs):
 	if 'actFunc' in kwargs:
 		actFunc = kwargs['actFunc']
@@ -60,19 +70,18 @@ def optimizer(inputs, weights, outputs, dx, **kwargs):
 		rate = kwargs['rate']
 	else:
 		rate = 0.1
-
-	neuronAmount, weightAmount = len(weights), len(weights[0])
-	totalWeightAmount = neuronAmount * weightAmount
-	
-	dxWeights = np.reshape(np.tile(np.append(dx, np.zeros((totalWeightAmount))), totalWeightAmount)[:totalWeightAmount ** 2], (neuronAmount, weightAmount, neuronAmount, weightAmount)) + weights
+		
+	dxWeights = dxMaker(weights, dx)
 	dxInputs = np.identity(len(inputs)) * dx + inputs
+	dxWeightsOutputs = layer(inputs, dxWeights, actFunc = actFunc)
+	dxInputsOutputs = layer(dxInputs, weights, actFunc = actFunc)
+	dxWeightsCosts = (outputs - dxWeightsOutputs) ** 2
+	dxInputsCosts = (outputs - dxInputsOutputs) ** 2
 
-	dxWeightsOutputs, dxInputsOutputs = layer(inputs, dxWeights, actFunc=actFunc), layer(dxInputs, weights, actFunc=actFunc)
-	dxWeightsCosts, dxInputsCosts = (outputs - dxWeightsOutputs) ** 2, (outputs - dxInputsOutputs) ** 2
-	
 	weightsGradients, inputsGradients = np.transpose((dxWeightsCosts - normalCost) / dx)[0], np.transpose((dxInputsCosts - normalCost) / dx)[0]
 
-	return weights - weightsGradients * rate, inputs - inputsGradients * rate
+	return [weights[0] - [row[0] for row in weightsGradients * rate]], inputs - inputsGradients * rate
+
 
 def backPropagation(inputs, weights, outputs, dx, **kwargs):
 	newWeights = deepcopy(weights)[::-1]
@@ -81,7 +90,8 @@ def backPropagation(inputs, weights, outputs, dx, **kwargs):
 	inps = neuralNetwork(inputs, weights)
 	networkInputs = inps[-2::-1] + [deepcopy(inputs)]
 
-	for currentLayer in range(len(networkInputs)-1):
+	for currentLayer in range(len(networkInputs)):
+		print('case')
 		newWeights[currentLayer], networkInputs[currentLayer] = optimizer(networkInputs[currentLayer], newWeights[currentLayer], newOutputs, dx, **kwargs)
 
 	return newWeights[::-1]
