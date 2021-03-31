2630d5d85bdbf357d1bf65c7745422c1b8051622
Ian Titor <ianpatricktan@gmail.com>
2021-03-31 00:07:59 -0400

    Ian's complete rewrite of the neural network module. Containing fixes, small patches and smaller quality of life changes. Unfortunately at the current time this is being commited there exists no documentation, but that may be added in the near future!

--- NNrewrite.py
+++ NNrewrite.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cupy as cp
 import _pickle as pkl
 from numba import jit
 
@@ -12,84 +13,59 @@ def softmax(x):
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
+
 def deepcopy(x):
-	return pkl.loads(pkl.dumps(x))
+    return pkl.loads(pkl.dumps(x))
 
 
-def generateWeights(layerData, inputQuantity, minimum = -1, maximum = 1):
-	layerDepthLimit = len(layerData)
-	augmentedLayerData = [inputQuantity] + deepcopy(layerData)
-	return [np.random.uniform(minimum, maximum, [layerData[layerDepth], augmentedLayerData[layerDepth] + 1]) - 0.5 for layerDepth in range(layerDepthLimit)]
+def cost(t, v):
+    return np.sum((t - v) ** 2, axis = -1)
+
+
+def karges(kwargs, check, default):
+    if check in kwargs:
+        return kwargs[check]
+    else:
+        return default
+
+
+def generateWeights(layerData, **kwargs):
+    minimum, maximum = karges(kwargs, 'minimum', -1), karges(kwargs, 'maximum', 1)
+    return [np.random.uniform(minimum, maximum, [layerData[1:][layer], layerData[layer] + 1]) for layer in range(len(layerData) - 1)]
 
 
 def layer(inputs, weights, **kwargs):
-	if 'actFunc' in kwargs:
-		actFunc = kwargs['actFunc']
-	else:
-		actFunc = sigmoid
-	shape = list(inputs.shape)[:-1] + [1]
-	biases = np.full(shape, 1)
-	outputs = actFunc(np.append(inputs, biases, axis = len(shape) - 1) @ weights.transpose(np.roll(np.arange(len(weights.shape)), -1)))
-	return outputs.transpose(np.roll(np.arange(len(outputs.shape)), 1))
+    return karges(kwargs, 'actFunc', sigmoid)(np.tensordot(np.append(inputs, np.full(list(inputs.shape)[:-1] + [1], 1), axis = -1), weights, axes = [[-1], [-1]]))
 
 
 def neuralNetwork(inputs, weights, **kwargs):
-	if 'actFunc' in kwargs:
-		actFunc = kwargs['actFunc']
-	else:
-		actFunc = sigmoid
-	if 'lastActFunc' in kwargs:
-		lastActFunc = kwargs['lastActFunc']
-	else:
-		lastActFunc = softmax
-	neuronOutputs = []
-	layerInputs = deepcopy(inputs)
-	for layerWeights in weights[:-1]:
-		layerInputs = layer(layerInputs, layerWeights, actFunc = actFunc)
-		neuronOutputs.append(deepcopy(layerInputs))
-	neuronOutputs.append(weights[-1])
-	return neuronOutputs
+    actFunc, finalActFunc = karges(kwargs, 'actFunc', sigmoid), karges(kwargs, 'finalActFunc', softmax)
+    neuronOutputs = []
+    layerInputs = deepcopy(inputs)
+    for layerWeights in weights[:-1]:
+        layerInputs = layer(layerInputs, layerWeights, actFunc = actFunc)
+        neuronOutputs.append(deepcopy(layerInputs))
+    
+    neuronOutputs.append(layer(layerInputs, weights[-1], actFunc = finalActFunc))
+    return neuronOutputs
 
 
-def dxMaker(weights, dx):
-	neuronAmount, weightAmount = len(weights), len(weights[0])
-	totalWeightAmount = neuronAmount * weightAmount
-	return np.reshape(np.tile(np.append(dx, np.zeros((totalWeightAmount))), totalWeightAmount)[:totalWeightAmount ** 2], (neuronAmount, weightAmount, neuronAmount, weightAmount)) + weights
+def generateDx(inputs, weights, **kwargs):
+    dx, neuronAmount, weightAmount = karges(kwargs, 'dx', 0.01), karges(kwargs, 'neuronAmount', len(weights)), karges(kwargs, 'weightAmount', len(weights[0]))
+    totalWeightAmount = neuronAmount * weightAmount
+    return np.identity(len(inputs)) * dx + inputs, np.reshape(np.tile(np.append(dx, np.zeros((totalWeightAmount))), totalWeightAmount)[:totalWeightAmount ** 2], (neuronAmount, weightAmount, neuronAmount, weightAmount)) + weights
 
 
-def optimizer(inputs, weights, outputs, dx, **kwargs):
-	if 'actFunc' in kwargs:
-		actFunc = kwargs['actFunc']
-	else:
-		actFunc = sigmoid
-	if 'normalCost' in kwargs:
-		normalCost = kwargs['normalCost']
-	else:
-		normalCost = (outputs - layer(inputs, weights, actFunc = actFunc)) ** 2
-	if 'rate' in kwargs:
-		rate = kwargs['rate']
-	else:
-		rate = 0.1
-		
-	dxWeights = dxMaker(weights, dx)
-	dxInputs = np.identity(len(inputs)) * dx + inputs
-	dxWeightsOutputs = layer(inputs, dxWeights, actFunc = actFunc)
-	dxInputsOutputs = layer(dxInputs, weights, actFunc = actFunc)
-	dxWeightsCosts = (outputs - dxWeightsOutputs) ** 2
-	dxInputsCosts = (outputs - dxInputsOutputs) ** 2
-
-	weightsGradients, inputsGradients = np.transpose((dxWeightsCosts - normalCost) / dx)[0], np.transpose((dxInputsCosts - normalCost) / dx)[0]
-
-	return weights - np.transpose(weightsGradients * rate), inputs - inputsGradients * rate
+def optimizer(inputs, weights, outputs, **kwargs):
+    rawCost, learningRate, dx = karges(kwargs, 'rawCost', cost(outputs, layer(inputs, weights, **kwargs))), karges(kwargs, 'learningRate', 0.1), karges(kwargs, 'dx', 0.01)
+    dxInputs, dxWeights = generateDx(inputs, weights, **kwargs)
+    return inputs - (cost(outputs, layer(dxInputs, weights, **kwargs)) - rawCost) / dx * learningRate, weights - (cost(outputs, layer(inputs, dxWeights, **kwargs)) - rawCost) / dx * learningRate
 
 
-def backPropagation(inputs, weights, outputs, dx, **kwargs):
-	newWeights = deepcopy(weights)[::-1]
-	newOutputs = deepcopy(outputs)
-	
-	inps = neuralNetwork(inputs, weights)
-	networkInputs = inps[-2::-1] + [deepcopy(inputs)]
-
-	for currentLayer in range(len(networkInputs)):
-		newWeights[currentLayer], networkInputs[currentLayer] = optimizer(networkInputs[currentLayer], newWeights[currentLayer], newOutputs, dx, **kwargs)
-	return newWeights[::-1]
+def backProp(inputs, weights, outputs, **kwargs):
+    layerInputs = neuralNetwork(inputs, weights, **kwargs)[:-1][::-1] + [inputs]
+    newWeights = deepcopy(weights)[::-1]
+    targetOutputs = outputs
+    for layerIndex, layerWeights in enumerate(newWeights):
+        targetOutputs, newWeights[layerIndex] = optimizer(layerInputs[layerIndex], layerWeights, targetOutputs, **kwargs)
+    return newWeights[::-1]
